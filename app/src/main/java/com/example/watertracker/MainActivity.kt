4d996c188001afc3d6866dcc3c30b3d3b1e52912
kicak <kicak@stud.uniza.sk>
2026-04-03 19:14:37 +0200

    test 2, aby som vedel ako to tu funguje

--- app/src/main/java/com/example/watertracker/MainActivity.kt
+++ app/src/main/java/com/example/watertracker/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             WaterTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "semestralna praca",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
